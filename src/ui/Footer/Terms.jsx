5fc8fdc64828bc7c32622daf6fb0e71390028154
niZaliznyak <novieasdf@gmail.com>
2022-07-13 12:15:37 +0300

    minor changes

--- src/ui/Footer/Terms.jsx
+++ src/ui/Footer/Terms.jsx
@@ -1,6 +1,6 @@
 export const Terms = () => {
     return (
-        <ul className="space-y-6 font-poppins font-normal text-xs md:flex">
+        <ul className="space-y-6 font-poppins font-normal text-xs md:flex md:space-y-0 md:mt-8 md:mb-2 md:space-x-5">
             <li>© Path 2022</li>
             <li>Terms of Service</li>
             <li>Privacy Policy</li>
--- src/ui/Footer/index.jsx
+++ src/ui/Footer/index.jsx
@@ -6,13 +6,18 @@ import { Terms } from "./Terms";
 export const Footer = () => {
 
     return (
-        <div className="w-full md:flex justify-between bg-primary p-4 text-white space-y-10">
-            <Contacts />
-            <ShortNav />
-            <div className="md:hidden">
+        <div className="w-full flex flex-col items-center bg-primary p-4 text-white">
+            <div className="w-full md:flex justify-between max-w-6xl space-y-10">
+                <Contacts />
+                <ShortNav />
+                <div className="md:hidden">
+                    <Terms />
+                </div>
+                <Subscribe />
+            </div>
+            <div className="hidden md:block w-full max-w-6xl">
                 <Terms />
             </div>
-            <Subscribe />
         </div>
     )
 };
