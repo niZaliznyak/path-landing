f7780272d066305cf12bb4570ef957f8f2d1ef32
niZaliznyak <novieasdf@gmail.com>
2022-07-13 12:07:31 +0300

    minor changes

--- src/ui/Footer/ShortNav.jsx
+++ src/ui/Footer/ShortNav.jsx
@@ -1,7 +1,7 @@
 export const ShortNav = () => {
     return (
         <div >
-            <p className="font-bold text-base mb-6 mt-8">Company</p>
+            <p className="font-bold text-base mb-6 mt-8 md:mt-0">Company</p>
             <ul className="space-y-4">
                 <li>Home</li>
                 <li>About Us</li>
--- src/ui/Footer/index.jsx
+++ src/ui/Footer/index.jsx
@@ -6,11 +6,13 @@ import { Terms } from "./Terms";
 export const Footer = () => {
 
     return (
-       <div className="w-full bg-primary p-4 text-white space-y-10">
+        <div className="w-full md:flex justify-between bg-primary p-4 text-white space-y-10">
             <Contacts />
             <ShortNav />
-            <Terms />
+            <div className="md:hidden">
+                <Terms />
+            </div>
             <Subscribe />
-       </div>
+        </div>
     )
 };
